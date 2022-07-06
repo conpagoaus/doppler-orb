061e734a24c735324e65b95e2aa589a97115687e
Maxim Filimonov <tpaktopsp@gmail.com>
2022-07-06 10:54:25 +0300

    update test deploy script

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-doppler secrets download -t "$DOPPLER_TOKEN" --no-file --format env --project "$DOPPLER_PROJECT" > doppler.env
+TOKEN=$DOPPLER_TOKEN_NAME
+doppler secrets download -t "${!TOKEN}" --no-file --format env --project "$DOPPLER_PROJECT" > doppler.env
 sed -e 's/^/export /' doppler.env >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
