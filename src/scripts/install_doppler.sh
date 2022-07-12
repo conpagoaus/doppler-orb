c252795f4afdf1d61e74aa4cb020f950bbe0bcc2
Emanuele Tonello <emanueletonello@gmail.com>
2022-07-12 10:21:08 +1000

    manually install sudo if not present

--- src/scripts/install_doppler.sh
+++ src/scripts/install_doppler.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set +e
-(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh
+
+apt-get install -y sudo
+ls -al /bin/sh && sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh && ls -al /bin/sh
+
+(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
 doppler --version
