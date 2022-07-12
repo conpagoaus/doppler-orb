cd4c57cc9cab46aa4fd8a6f5cac711f5315eeb74
Emanuele Tonello <emanueletonello@gmail.com>
2022-07-12 10:10:08 +1000

    sudo not needed on circle ci

--- src/scripts/install_doppler.sh
+++ src/scripts/install_doppler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set +e
-(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
+(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh
 doppler --version
