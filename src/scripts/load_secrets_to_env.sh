d9f6d486ba919293c6a2c3bccc5816428196b6b2
Emanuele Tonello <emanueletonello@gmail.com>
2022-08-16 15:13:50 +1000

    put the secrets format under a parameter

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOKEN=$DOPPLER_TOKEN_NAME
-doppler secrets download -t "${!TOKEN}" --no-file --format env --project "$DOPPLER_PROJECT" > doppler.env
+doppler secrets download -t "${!TOKEN}" --no-file --format "$DOPPLER_FORMAT" --project "$DOPPLER_PROJECT" > doppler.env
 sed -e 's/^/export /' doppler.env >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
