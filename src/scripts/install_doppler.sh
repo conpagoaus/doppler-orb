421c07dd51c7eef0e3aca19d6cdb93a623cc19ed
Maxim Filimonov <tpaktopsp@gmail.com>
2022-10-18 13:31:56 +0800

    add installation of gnupg before doppler

--- src/scripts/install_doppler.sh
+++ src/scripts/install_doppler.sh
@@ -9,5 +9,21 @@ then
   ls -al /bin/sh && sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh && ls -al /bin/sh
 fi
 
+# Install gnupg if its not present
+if ! command -v gpg &> /dev/null
+then
+  # install gnupg
+  if command -v apt-get &> /dev/null
+  then
+    sudo apt-get install -y gnupg
+  elif command -v brew &> /dev/null
+  then
+    brew install gnupg
+  else
+    echo "Please install gnupg before installing doppler. Unable to detect apt-get or homebrew"
+    exit 1
+  fi
+fi
+
 (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
 doppler --version
